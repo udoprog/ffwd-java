0e25536a28f4ebaa4a1930754812e92e174388a2
John-John Tedro <udoprog@spotify.com>
2017-10-09 14:55:41 +0200

    more protocol decoding

--- modules/http/src/main/java/com/spotify/ffwd/http/HttpDecoder.java
+++ modules/http/src/main/java/com/spotify/ffwd/http/HttpDecoder.java
@@ -15,42 +15,52 @@
  */
 package com.spotify.ffwd.http;
 
-import com.google.protobuf250.InvalidProtocolBufferException;
-import com.spotify.ffwd.model.Event;
-import com.spotify.ffwd.model.Metric;
-import com.spotify.ffwd.protocol0.Protocol0;
-import io.netty.buffer.ByteBuf;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.netty.buffer.ByteBufInputStream;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandler.Sharable;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToMessageDecoder;
 import io.netty.handler.codec.http.DefaultFullHttpResponse;
-import io.netty.handler.codec.http.HttpMessage;
-import io.netty.handler.codec.http.HttpResponse;
+import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.HttpResponseStatus;
 import io.netty.handler.codec.http.HttpVersion;
-import lombok.extern.slf4j.Slf4j;
-
 import java.io.InputStream;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import javax.inject.Inject;
+import javax.inject.Named;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Sharable
-public class HttpDecoder extends MessageToMessageDecoder<HttpMessage> {
+public class HttpDecoder extends MessageToMessageDecoder<FullHttpRequest> {
     public static final int MAX_FRAME_SIZE = 0xffffff;
 
+    @Inject
+    @Named("application/json")
+    private ObjectMapper mapper;
+
     @Override
-    protected void decode(ChannelHandlerContext ctx, HttpMessage in, List<Object> out)
+    protected void decode(ChannelHandlerContext ctx, FullHttpRequest in, List<Object> out)
         throws Exception {
 
-        log.info("HttpMessage: {}", in);
+        final Batch batch;
+        try (final InputStream inputStream = new ByteBufInputStream(in.content())) {
+            batch = mapper.readValue(inputStream, Batch.class);
+        }
 
-        out.add(new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK));
+        log.info("HttpRequest: {}", in.content());
+        log.info("Batch: {}", batch);
+
+        ctx.channel()
+           .writeAndFlush(new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK))
+           .addListener(new ChannelFutureListener() {
+               @Override
+               public void operationComplete(final ChannelFuture future) throws Exception {
+                   future.channel()
+                         .close();
+               }
+           });
     }
-
 }
--- modules/http/src/main/java/com/spotify/ffwd/http/HttpProtocolServer.java
+++ modules/http/src/main/java/com/spotify/ffwd/http/HttpProtocolServer.java
@@ -18,16 +18,26 @@ package com.spotify.ffwd.http;
 import com.google.inject.Inject;
 import com.spotify.ffwd.protocol.ProtocolServer;
 import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandler;
+import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelInitializer;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpRequestDecoder;
 import io.netty.handler.codec.http.HttpResponseEncoder;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpVersion;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Decode individual frames, should only be used with UDP protocols.
  *
  * @author udoprog
  */
+@Slf4j
 public class HttpProtocolServer implements ProtocolServer {
     @Inject
     private ChannelInboundHandler handler;
@@ -40,7 +50,32 @@ public class HttpProtocolServer implements ProtocolServer {
         return new ChannelInitializer<Channel>() {
             @Override
             protected void initChannel(Channel ch) throws Exception {
-                ch.pipeline().addLast(new HttpRequestDecoder(), decoder, new HttpResponseEncoder());
+                final ChannelInboundHandlerAdapter handler = new ChannelInboundHandlerAdapter() {
+                    @Override
+                    public void exceptionCaught(final ChannelHandlerContext ctx,
+                                                final Throwable cause
+                    ) throws Exception {
+                        log.error("Exception: ", cause);
+                        ctx.channel()
+                           .writeAndFlush(new DefaultFullHttpResponse(HttpVersion.HTTP_1_1,
+                                                                      HttpResponseStatus
+                                                                          .INTERNAL_SERVER_ERROR))
+                           .addListener(new ChannelFutureListener() {
+                               @Override
+                               public void operationComplete(final ChannelFuture future)
+                                   throws Exception {
+                                   future.channel()
+                                         .close();
+                               }
+                           });
+                    }
+                };
+                ch.pipeline()
+                  .addLast(new HttpRequestDecoder(), new HttpObjectAggregator(Integer.MAX_VALUE), decoder);
+                ch.pipeline()
+                  .addLast(new HttpResponseEncoder());
+                ch.pipeline()
+                  .addLast(handler);
             }
         };
     }
