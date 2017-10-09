e8e77b601cfcaf16890241f883bccd609f0d51fe
Gabriel Gerhardsson <gabrielg@spotify.com>
2017-10-09 11:31:44 +0200

    WIP

---
+++ modules/http/src/main/java/com/spotify/ffwd/http/HttpDecoder.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2013-2017 Spotify AB. All rights reserved.
+ *
+ * The contents of this file are licensed under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.spotify.ffwd.http;
+
+import com.google.protobuf250.InvalidProtocolBufferException;
+import com.spotify.ffwd.model.Event;
+import com.spotify.ffwd.model.Metric;
+import com.spotify.ffwd.protocol0.Protocol0;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufInputStream;
+import io.netty.channel.ChannelHandler.Sharable;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToMessageDecoder;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.HttpMessage;
+import io.netty.handler.codec.http.HttpResponse;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpVersion;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.InputStream;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@Slf4j
+@Sharable
+public class HttpDecoder extends MessageToMessageDecoder<HttpMessage> {
+    public static final int MAX_FRAME_SIZE = 0xffffff;
+
+    @Override
+    protected void decode(ChannelHandlerContext ctx, HttpMessage in, List<Object> out)
+        throws Exception {
+
+        log.info("HttpMessage: {}", in);
+
+        out.add(new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK));
+    }
+
+}
---
+++ modules/http/src/main/java/com/spotify/ffwd/http/HttpProtocolServer.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2013-2017 Spotify AB. All rights reserved.
+ *
+ * The contents of this file are licensed under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.spotify.ffwd.http;
+
+import com.google.inject.Inject;
+import com.spotify.ffwd.protocol.ProtocolServer;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInboundHandler;
+import io.netty.channel.ChannelInitializer;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
+
+/**
+ * Decode individual frames, should only be used with UDP protocols.
+ *
+ * @author udoprog
+ */
+public class HttpProtocolServer implements ProtocolServer {
+    @Inject
+    private ChannelInboundHandler handler;
+
+    @Inject
+    private HttpDecoder decoder;
+
+    @Override
+    public final ChannelInitializer<Channel> initializer() {
+        return new ChannelInitializer<Channel>() {
+            @Override
+            protected void initChannel(Channel ch) throws Exception {
+                ch.pipeline().addLast(new HttpRequestDecoder(), decoder, new HttpResponseEncoder());
+            }
+        };
+    }
+}
